4526ed2361ca391c54cbbe33f829777e0a29f361
James Pedigo <ogidep.m.semaj@gmail.com>
2026-06-01 09:20:24 -0500

    Removes unneeded constraints

--- db/schema.sql
+++ db/schema.sql
@@ -2,9 +2,9 @@ DROP TABLE IF EXISTS employees;
 
 CREATE TABLE employees (
     id serial PRIMARY KEY,
-    name text NOT NULL UNIQUE,
-    birthday date NOT NULL,
-    salary int NOT NULL
+    name text,
+    birthday date,
+    salary int
     )
 
 
