3270f40dd0279db381358fc00e0befeb383d1e74
James Pedigo <ogidep.m.semaj@gmail.com>
2026-05-31 19:41:57 -0500

    adds Create table employees in schema.sql

--- db/schema.sql
+++ db/schema.sql
@@ -1,3 +1,10 @@
 DROP TABLE IF EXISTS employees;
 
+CREATE TABLE employees (
+    id serial PRIMARY KEY,
+    name text NOT NULL UNIQUE,
+    birthday date NOT NULL,
+    salary int NOT NULL
+    )
+
 
