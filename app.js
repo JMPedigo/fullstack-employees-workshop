0f6dd1517fddeb95b10197205725ecc7c9672f80
James Pedigo <ogidep.m.semaj@gmail.com>
2026-06-02 18:15:04 -0500

    Adds body parsing middleware

--- app.js
+++ app.js
@@ -2,6 +2,17 @@ import express from "express";
 const app = express();
 export default app;
 
+import employeesRouter from "#api/employees";
+
+app.use(express.json());
+
+app.use("/employees", employeesRouter);
+
 app.get("/", (req, res) => {
   res.send("Welcome to the Fullstack Employees API.");
+});
+
+app.use((err, req, res, next) => {
+  console.error(err);
+  res.status(500).send("Sorry! Something went wrong :(");
 });
